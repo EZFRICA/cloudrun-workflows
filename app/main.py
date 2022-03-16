372c443b3173129811c1d1121b967589ac2b1de2
EZFRICA <ricardobokove@gmail.com>
2022-03-16 19:28:27 +0100

    update code

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@
 
 from fastapi import FastAPI
 import os
-# import time
+import time
 
 from google.cloud import workflows_v1beta
 from google.cloud.workflows import executions_v1beta
@@ -51,21 +51,21 @@ async def execute_workflow():
     response = execution_client.create_execution(request={"parent": parent})
     print(f"Created execution: {response.name}")
 
-    # # Wait for execution to finish, then print results.
-    # execution_finished = False
-    # backoff_delay = 1  # Start wait with delay of 1 second
-    # print('Poll every second for result...')
-    # while (not execution_finished):
-    #     execution = execution_client.get_execution(request={"name": response.name})
-    #     execution_finished = execution.state != executions.Execution.State.ACTIVE
+    # Wait for execution to finish, then print results.
+    execution_finished = False
+    backoff_delay = 1  # Start wait with delay of 1 second
+    print('Poll every second for result...')
+    while (not execution_finished):
+        execution = execution_client.get_execution(request={"name": response.name})
+        execution_finished = execution.state != executions.Execution.State.ACTIVE
 
-    #     # If we haven't seen the result yet, wait a second.
-    #     if not execution_finished:
-    #         print('- Waiting for results...')
-    #         time.sleep(backoff_delay)
-    #         backoff_delay *= 2  # Double the delay to provide exponential backoff.
-    #     else:
-    #         print(f'Execution finished with state: {execution.state.name}')
-    #         print(execution.result)
-    #         return execution.result
-    # # [END workflows_api_quickstart]
+        # If we haven't seen the result yet, wait a second.
+        if not execution_finished:
+            print('- Waiting for results...')
+            time.sleep(backoff_delay)
+            backoff_delay *= 2  # Double the delay to provide exponential backoff.
+        else:
+            print(f'Execution finished with state: {execution.state.name}')
+            print(execution.result)
+            return execution.result
+    # [END workflows_api_quickstart]
